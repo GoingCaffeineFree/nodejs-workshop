fa66ae1fc68563c44f0aa8615b27ac77a0209c7d
Jerome Ng <jerome2116@hotmail.com>
2022-12-13 18:54:38 +0800

    Adds express.js Hello World

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Hello World!")
+import cors from "cors";
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+const port = process.env.port ?? 8080;
+
+// Middleware
+app.use(morgan("combined"));
+app.use(cors());
+app.use(express.json());
+
+// GET Route
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello World!" });
+});
+
+// Open listening port for connection
+app.listen(port, (err) => {
+  if (err) console.error(err);
+  console.log(`NodeJS listening on port ${port}`);
+});
